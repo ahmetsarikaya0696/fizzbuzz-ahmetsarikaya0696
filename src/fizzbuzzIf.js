89990a21b8a1df97d0c3d43bd4878b0aeb994e3c
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2025-05-25 21:54:17 +0300

    fizzBuzz

--- src/fizzbuzzIf.js
+++ src/fizzbuzzIf.js
@@ -1,5 +1,17 @@
 function fizzbuzzIf(num) {
-    // Kodlar buraya gelecek
+    if (typeof num !== 'number') {
+        return 'Input must be a number';
+    }
+    
+    if (num % 3 === 0 && num % 5 === 0) {
+        return 'FizzBuzz';
+    } else if (num % 3 === 0) {
+        return 'Fizz';
+    } else if (num % 5 === 0) {
+        return 'Buzz';
+    } else {
+        return num;
+    }
 }
 
 module.exports = fizzbuzzIf;
--- src/fizzbuzzSwitch.js
+++ src/fizzbuzzSwitch.js
@@ -1,5 +1,18 @@
 function fizzbuzzSwitch(num) {
-    // Kodlar buraya gelecek
+    if (typeof num !== 'number') {
+        return 'Input must be a number';
+    }
+
+    switch (true) {
+        case num % 3 === 0 && num % 5 === 0:
+            return 'FizzBuzz';
+        case num % 3 === 0:
+            return 'Fizz';
+        case num % 5 === 0:
+            return 'Buzz';
+        default:
+            return num;
+    }
 }
 
 module.exports = fizzbuzzSwitch;
